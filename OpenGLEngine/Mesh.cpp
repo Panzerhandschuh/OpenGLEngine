cdcd66779ce6fe186540c59a926f09bb7276d742
Panzerhandschuh <TylerBeff@gmail.com>
2015-11-04 00:57:27 -0500

    Cleaned/optimized mesh class
    Also fixed a missing glDeleteBuffers for vboNormals

--- OpenGLEngine/Mesh.cpp
+++ OpenGLEngine/Mesh.cpp
@@ -6,106 +6,92 @@
 Mesh::Mesh()
 {
 	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vboVerts);
+	glGenBuffers(1, &vboNormals);
+	glGenBuffers(1, &vboUvs);
+	glGenBuffers(1, &vboColors);
+	glGenBuffers(1, &ebo);
+
+	// Configure shader attributes
+	glBindVertexArray(vao); // Bind vao
+
+	// Vertices
+	glBindBuffer(GL_ARRAY_BUFFER, vboVerts); // Bind vbo
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // Configure vertex attribute
+	glEnableVertexAttribArray(0); // Enable vertex attribute 0 to be used by vertex shader
+
+	// Normals
+	glBindBuffer(GL_ARRAY_BUFFER, vboNormals); // Bind vbo
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0); // Configure normal attribute
+	glEnableVertexAttribArray(1); // Enable vertex attribute 1 to be used by vertex shader
+
+	// Uvs
+	glBindBuffer(GL_ARRAY_BUFFER, vboUvs); // Bind vbo
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0); // Configure uv attribute
+	glEnableVertexAttribArray(2); // Enable vertex attribute 2 to be used by vertex shader
+
+	// Colors
+	glBindBuffer(GL_ARRAY_BUFFER, vboColors); // Bind vbo
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, 0); // Configure vertex color attribute
+	glEnableVertexAttribArray(3); // Enable vertex attribute 3 to be used by vertex shader
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind vbo
+	glBindVertexArray(0); // Unbind vao
 }
 
 Mesh::~Mesh()
 {
 	glDeleteVertexArrays(1, &vao);
 	glDeleteBuffers(1, &vboVerts);
-	glDeleteBuffers(1, &ebo);
+	glDeleteBuffers(1, &vboNormals);
 	glDeleteBuffers(1, &vboUvs);
 	glDeleteBuffers(1, &vboColors);
+	glDeleteBuffers(1, &ebo);
 }
 
 void Mesh::SetVertices(vector<glm::vec3> verts)
 {
 	this->vertices = verts;
 
-	glGenBuffers(1, &vboVerts);
-
-	glBindVertexArray(vao); // Bind vao
 	glBindBuffer(GL_ARRAY_BUFFER, vboVerts); // Bind vbo
-
 	glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(glm::vec3), &this->vertices[0], GL_STATIC_DRAW); // Set the vertices that vbo should use
-
-	// Shader attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // Configure vertex attribute
-	glEnableVertexAttribArray(0); // Enable vertex attribute 0 to be used by vertex shader
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind vbo
-	glBindVertexArray(0); // Unbind vao
 }
 
 void Mesh::SetNormals(vector<glm::vec3> normals)
 {
 	this->normals = normals;
 
-	glGenBuffers(1, &vboNormals);
-
-	glBindVertexArray(vao); // Bind vao
 	glBindBuffer(GL_ARRAY_BUFFER, vboNormals); // Bind vbo
-
 	glBufferData(GL_ARRAY_BUFFER, this->normals.size() * sizeof(glm::vec3), &this->normals[0], GL_STATIC_DRAW); // Set the normals that vbo should use
-
-	// Shader attributes
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0); // Configure vertex attribute
-	glEnableVertexAttribArray(1); // Enable vertex attribute 1 to be used by vertex shader
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind vbo
-	glBindVertexArray(0); // Unbind vao
 }
 
 void Mesh::SetUvs(vector<glm::vec2> uvs)
 {
 	this->uvs = uvs;
 
-	glGenBuffers(1, &vboUvs);
-
-	glBindVertexArray(vao); // Bind vao
 	glBindBuffer(GL_ARRAY_BUFFER, vboUvs); // Bind vbo
-
-	glBufferData(GL_ARRAY_BUFFER, this->uvs.size() * sizeof(glm::vec2), &this->uvs[0], GL_STATIC_DRAW); // Set tex coords
-
-	// Shader attributes
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0); // Configure tex coords attribute
-	glEnableVertexAttribArray(2); // Enable vertex attribute 2 to be used by vertex shader
-
+	glBufferData(GL_ARRAY_BUFFER, this->uvs.size() * sizeof(glm::vec2), &this->uvs[0], GL_STATIC_DRAW); // Set the uvs that vbo should use
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind vbo
-	glBindVertexArray(0); // Unbind vao
-}
-
-void Mesh::SetIndices(vector<GLuint> indices)
-{
-	this->indices = indices;
-
-	glGenBuffers(1, &ebo);
-
-	glBindVertexArray(vao); // Bind vao
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); // Bind ebo
-
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &this->indices[0], GL_STATIC_DRAW); // Set indices
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Unbind ebo
-	glBindVertexArray(0); // Unbind vao
 }
 
 void Mesh::SetVertexColors(vector<glm::vec3> colors)
 {
 	this->colors = colors;
 
-	glGenBuffers(1, &vboColors);
-
-	glBindVertexArray(vao); // Bind vao
 	glBindBuffer(GL_ARRAY_BUFFER, vboColors); // Bind vbo
-
-	glBufferData(GL_ARRAY_BUFFER, this->colors.size() * sizeof(glm::vec3), &this->colors[0], GL_STATIC_DRAW); // Set the vertices that vbo should use
-
-	// Shader attributes
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, 0); // Configure vertex color attribute
-	glEnableVertexAttribArray(3); // Enable vertex attribute 3 to be used by vertex shader
-
+	glBufferData(GL_ARRAY_BUFFER, this->colors.size() * sizeof(glm::vec3), &this->colors[0], GL_STATIC_DRAW); // Set the colors that vbo should use
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind vbo
-	glBindVertexArray(0); // Unbind vao
+}
+
+void Mesh::SetIndices(vector<GLuint> indices)
+{
+	this->indices = indices;
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); // Bind ebo
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices.size() * sizeof(GLuint), &this->indices[0], GL_STATIC_DRAW); // Set the indices that ebo should use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Unbind ebo
 }
 
 void Mesh::SetTexture(const char* filePath)
